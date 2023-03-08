676473833a3bdf3350a13cc1faafb008d3d59495
Wandonium <hillarywando@gmail.com>
2023-03-08 13:02:11 +0300

    Added use of the bean ID from the XML resource file when createing objects using dependency injection.

---
+++ src/main/java/com/wandonium/springframeworkdemo/Nurse.java
@@ -0,0 +1,11 @@
+package com.wandonium.springframeworkdemo;
+
+/**
+ *
+ * @author Futuristic Ltd
+ */
+public class Nurse {
+    public void assist() {
+        System.out.println("Nurse is assisting");
+    }
+}
--- src/main/java/com/wandonium/springframeworkdemo/SpringFrameworkDemo.java
+++ src/main/java/com/wandonium/springframeworkdemo/SpringFrameworkDemo.java
@@ -12,9 +12,15 @@ public class SpringFrameworkDemo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
-        ApplicationContext context = new ClassPathXmlApplicationContext();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         
-        Doctor doctor = new Doctor();
+        //Doctor doctor = new Doctor();
+        // Creating object using Spring's XML-based dependency injection.
+        Doctor doctor = context.getBean(Doctor.class);
         doctor.assist();
+        
+        // Making use of the XML id from the spring.xml resource
+        Nurse nurse = (Nurse) context.getBean("nurse");
+        nurse.assist();
     }
 }
